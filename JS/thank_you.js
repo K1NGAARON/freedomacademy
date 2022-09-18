d1b87ec00ecf94b6b9ea052a18a157b0e12aa41f
K1NGAARON <gerardaaron1997@gmail.com>
2022-09-18 20:37:47 +0200

    alert box with url copy confirmation

--- JS/thank_you.js
+++ JS/thank_you.js
@@ -2,6 +2,8 @@ function copyURL() {
     // Get the text field
     var copyText = "https://freedomacademy.com";
     navigator.clipboard.writeText(copyText);
+
+    alert('The URL has been copied.')
 }
 
 
