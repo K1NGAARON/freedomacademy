14865caff68366d9327cf18be37b33c9f33a6bf8
K1NGAARON <gerardaaron1997@gmail.com>
2022-09-25 13:42:47 +0200

    changes

--- JS/thank_you.js
+++ JS/thank_you.js
@@ -11,10 +11,3 @@ function copyToClipboard() {
 $('#copyURL').click(function(){
     copyToClipboard();
 });
-
-
-// To get the most out of this experience, join the Facebook Group. This is where you'll get access to our live Q&A sessions plus special bonus trainings leading up to the call.
-
-// Plus it's where most guys find motivation and inspiration to take action by connecting with others inside the community.
-
-// Click below to join the group now and then make a post telling your new to the #FreedomCommunity where you're from, the #1 reason why you booked a call, and what you want to get out of it.
