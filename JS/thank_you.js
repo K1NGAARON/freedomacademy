b54d51ecd3bfe2dc089830bba3b75677ab49db5e
K1NGAARON <gerardaaron1997@gmail.com>
2022-09-30 07:52:33 +0200

    added social sharing text

--- JS/thank_you.js
+++ JS/thank_you.js
@@ -1,11 +1,5 @@
 function copyToClipboard() {
-    // var copyText = "Just signed up for a Freedom Call! ⚡️Let's grow together and watch the video below, in case you want to learn how to start & scale your own online service business from scratch, step-by step! #FreedomAcademy"
-
-    var copyText = `
-            Just signed up for a Freedom Call! ⚡️Let's grow together and watch the video below, in case you want to learn how to start & scale your own online service business from scratch, step-by step! #FreedomAcademy
-
-            https://freedomacademy.be
-        `;
+    var copyText = "Just signed up for a Freedom Call! ⚡️Let's grow together and watch the video below, in case you want to learn how to start & scale your own online service business from scratch, step-by step! #FreedomAcademy"
 
     navigator.clipboard.writeText(copyText).then(() => {
         // Alert the user that the action took place.
