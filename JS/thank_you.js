3261cd7b6ad14de5a77e0e4d6c1445ded9f676cc
K1NGAARON <gerardaaron1997@gmail.com>
2022-09-30 21:02:32 +0200

    script

--- JS/thank_you.js
+++ JS/thank_you.js
@@ -1,5 +1,5 @@
 function copyToClipboard() {
-    var copyText = "Just signed up for a Freedom Call! ⚡️Let's grow together and watch the video below, in case you want to learn how to start & scale your own online service business from scratch, step-by step! #FreedomAcademy"
+    var copyText = "https://freedomacademy.be/thank-you"
 
     navigator.clipboard.writeText(copyText).then(() => {
         // Alert the user that the action took place.
