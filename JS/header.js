7710cd84860b7490ac9d9c6954e004acb80e05cc
K1NGAARON <gerardaaron1997&gmail.com>
2022-12-29 15:28:25 +0100

    changes

---
+++ JS/header.js
@@ -0,0 +1,13 @@
+const header = document.querySelector('#header');
+const sticky = header.offsetTop;
+
+function makeSticky() {
+    console.log('TEST');
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+};
+
+// document.addEventListener('scroll', makeSticky);
