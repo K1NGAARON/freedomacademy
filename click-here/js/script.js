3d22b9fe9d533aaae0541585e23a9beced28d42d
K1NGAARON <gerardaaron1997&gmail.com>
2023-02-19 19:16:11 +0100

    changes

--- click-here/js/script.js
+++ click-here/js/script.js
@@ -6,6 +6,10 @@ const image = document.querySelector('#image')
 
 function openModal(e) {
     modal.style.display = "block";
+    $('html, body').css({
+        overflow: 'hidden',
+        height: '100%'
+    });
 }
 
 // When the user clicks on the button, open the modal
@@ -14,10 +18,18 @@ image.addEventListener('click', openModal);
 
 span.addEventListener('click', function(e) {
     modal.style.display = "none";
+    $('html, body').css({
+        overflow: 'auto',
+        height: 'auto'
+    });
 });
 
 window.addEventListener('click', function(e) {
     if (e.target == modal) {
         modal.style.display = "none";
+        $('html, body').css({
+            overflow: 'auto',
+            height: 'auto'
+        });
       }
 });
