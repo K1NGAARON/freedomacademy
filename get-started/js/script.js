0da2ec7d4aee9644217d9b80840ff10e11db0b98
K1NGAARON <gerardaaron1997@gmail.com>
2023-04-01 14:25:28 +0200

    get started page

---
+++ get-started/js/script.js
@@ -0,0 +1,37 @@
+// Get the modal
+const modal = document.getElementById("myModal");
+const modalBtn = document.getElementById("popup");
+const secondBtn = document.getElementById("secondBtn");
+const span = document.getElementById("closeModal");
+const image = document.querySelector('#image')
+
+function openModal(e) {
+    modal.style.display = "block";
+    $('html, body').css({
+        overflow: 'hidden',
+        height: '100%'
+    });
+}
+
+// When the user clicks on the button, open the modal
+modalBtn.addEventListener('click', openModal);
+image.addEventListener('click', openModal);
+secondBtn.addEventListener('click', openModal);
+
+span.addEventListener('click', function(e) {
+    modal.style.display = "none";
+    $('html, body').css({
+        overflow: 'auto',
+        height: 'auto'
+    });
+});
+
+window.addEventListener('click', function(e) {
+    if (e.target == modal) {
+        modal.style.display = "none";
+        $('html, body').css({
+            overflow: 'auto',
+            height: 'auto'
+        });
+      }
+});
